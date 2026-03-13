81bedad218f75dee5a0d845f73f849c8b532c73c
Kamran Wajdani <hello@zuz.com.pk>
2026-03-14 00:04:03 +0500

    v0.1.17

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,8 +1,14 @@
 "use client"
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 const useDebounce = <T extends (...args: any[]) => void>(func: T, delay: number) => {
     const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+    useEffect(() => {
+        return () => {
+            if (timeoutRef.current) clearTimeout(timeoutRef.current);
+        };
+    }, []);
 
     const debouncedFunction = useCallback((...args: Parameters<T>) => {
         if (timeoutRef.current) {
